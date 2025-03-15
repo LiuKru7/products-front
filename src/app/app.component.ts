67f7b309ff12f9794b5869455d2cca44e0e25cc1
liudvikaskruopis <liukru7@gmail.com>
2025-03-15 12:17:29 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Product} from "./model/product";
+import {ProductServiceService} from "./service/product-service.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'products-front';
+export class AppComponent implements OnInit {
+
+  shoes: Product[] = [];
+  jackets: Product[] = [];
+
+  constructor(private productService: ProductServiceService) {
+  }
+
+  ngOnInit() {
+    this.shoes = this.productService.getShoes();
+    this.productService.getJackets().subscribe({
+      next: (data: Product[]) => {
+        this.jackets = data;
+      }
+    });
+  }
 }
---
+++ src/app/service/product-service.service.ts
@@ -0,0 +1,41 @@
+import {Injectable} from '@angular/core';
+import {Product} from "../model/product";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductServiceService {
+  private jacketApiUrl = 'http://localhost:8080/api/product/jacket';
+
+  constructor(private http: HttpClient) {
+  }
+
+  private shoes: Product[] = [
+    {
+      id: 1,
+      name: "Running shoes",
+      price: 19.99,
+      category: "shoes",
+      description: "Very comfortable shoes",
+      inStock: true
+    },
+    {
+      id: 2,
+      name: "Hiking shoes",
+      price: 19.99,
+      category: "shoes",
+      description: "Waterproof nice hiking shoes",
+      inStock: true
+    }
+  ]
+
+  getShoes(): Product[] {
+    return this.shoes
+  }
+
+  public getJackets(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.jacketApiUrl)
+  };
+}
